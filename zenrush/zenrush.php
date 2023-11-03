7058782fce3662fcfca3085f4f5010f4ab23b9f5
Kevin Koester <kevin.koester@zenfulfillment.com>
2023-11-03 22:41:44 +0100

    :bookmark: v1.2.13

--- zenrush/zenrush.php
+++ zenrush/zenrush.php
@@ -12,7 +12,7 @@
  * Plugin Name:       Zenrush
  * Plugin URI:        https://github.com/zenfulfillment/zenrush-wp-plugin
  * Description:       Integration Plugin for Zenrush Premium Delivery
- * Version:           1.2.12
+ * Version:           1.2.13
  * Author:            Zenfulfillment
  * Author URI:        https://zenfulfillment.com
  * License:           No License
@@ -35,7 +35,7 @@ if ( !defined( 'WPINC' ) ) {
  * 
  * @since   1.0.0
  */
-const ZENRUSH_VERSION = '1.2.12';
+const ZENRUSH_VERSION = '1.2.13';
 
 /**
  * Plugin file path.
